e0c92c1a30cc0505ef74f4781374e00cd5a4983d
Rahul Tiwari <rtiwari@tavisca.in>
2019-06-18 16:57:22 +0530

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -1,2 +1,4 @@
-
-# Default is "World"name = ARGV.first || "World"puts "Hello, #{name}!"
+#Default is World
+# Author: Jim Weirich(jim@somewhere.com)
+name = ARGV.first || "World"
+puts "Hello, #{name}!"
